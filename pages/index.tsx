a83525ffbc0195e4671b6eb8058ba9698af9e0b6
Olivier Dorvelus <ollydorvelus@gmail.com>
2022-01-30 16:02:24 -0500

    material UI & axios

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
+import Button from '@mui/material/Button';
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <h1>Poet Editor</h1>
+      <Button variant="contained">Button</Button>
     </div>
   )
 }
